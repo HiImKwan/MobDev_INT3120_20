6e5da3a5c2c4e3735f9612ad1930922210cfbe3e
Kwan <nhimcoi262@gmail.com>
2023-09-21 06:08:06 +0700

    Lesson 6

--- Lesson6/app/src/main/java/com/example/myapplication/MainActivity.java
+++ Lesson6/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,8 +1,6 @@
 package com.example.myapplication;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.view.MenuItemCompat;
 
 import android.os.Bundle;
 import android.view.ContextMenu;
@@ -11,19 +9,48 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
-import android.widget.SearchView;
+import android.widget.PopupMenu;
 import android.widget.Toast;
+import androidx.appcompat.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
-
-    Button button;
+    Button button1, button2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        this.button = this.findViewById(R.id.button1);
-        this.registerForContextMenu(this.button);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+
+        setSupportActionBar(toolbar);
+
+        this.button1 = this.findViewById(R.id.button1);
+        this.button2 = this.findViewById(R.id.button2);
+        this.registerForContextMenu(this.button1);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showPopupMenu();
+            }
+        });
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.option_menu, menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int itemId = item.getItemId();
+        if (itemId == R.id.option1) {
+            Toast.makeText(MainActivity.this, item.getTitle(), Toast.LENGTH_LONG).show();
+            return true;
+        } else if (itemId == R.id.option2) {
+            Toast.makeText(MainActivity.this, item.getTitle(), Toast.LENGTH_LONG).show();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
     @Override
     public void onCreateContextMenu(ContextMenu menu, View view, ContextMenu.ContextMenuInfo menuInfo){
@@ -32,4 +59,28 @@ public class MainActivity extends AppCompatActivity {
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.layout_context_menu, menu);
     }
+    public boolean onContextItemSelected(MenuItem item) {
+        int itemId = item.getItemId();
+        if (itemId == R.id.option1) {
+            Toast.makeText(MainActivity.this, item.getTitle(), Toast.LENGTH_LONG).show();
+        } else if (itemId == R.id.option2) {
+            Toast.makeText(MainActivity.this, item.getTitle(), Toast.LENGTH_LONG).show();
+        }
+        return super.onContextItemSelected(item);
+    }
+
+    private void showPopupMenu() {
+        PopupMenu popupMenu = new PopupMenu(MainActivity.this, this.button2);
+        popupMenu.inflate(R.menu.layout_popup_menu);
+        Menu menu = popupMenu.getMenu();
+        popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                Toast.makeText(MainActivity.this, item.getTitle(), Toast.LENGTH_LONG).show();
+                return false;
+            }
+        });
+        popupMenu.show();
+    }
+
 }
